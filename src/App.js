bdb0fed6dd99c2889f925cbe1dd7a3ed18284cbe
AshabAsir <ashabasir010@gmail.com>
2022-05-11 18:42:55 +0600

    all pages setup in router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import "./App.css";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Reviews from "./Pages/Reviews/Reviews";
+import Contact from "./Pages/Contact/Contact";
+import Login from "./Pages/Login/Login";
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment></Appointment>} />
+        <Route path="/reviews" element={<Reviews></Reviews>} />
+        <Route path="/contact" element={<Contact></Contact>} />
+        <Route path="/login" element={<Login></Login>} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>This login Page.</h1>
+    </div>
+  );
+};
+
+export default Login;
